a1a0e4526decb3ac27b4ec3776a3f428c5535409
RodionovDenis <denisrodionov5@gmail.com>
2020-05-23 02:19:11 +0300

    lab3

---
+++ Raytracing/Raytracing/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace OpenGL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.glControl1 = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(9, 6);
+            this.glControl1.Margin = new System.Windows.Forms.Padding(1);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(530, 424);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 440);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Трассировка лучей";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private OpenTK.GLControl glControl1;
+    }
+}
+
---
+++ Raytracing/Raytracing/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace OpenGL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int BasicProgramID; // Номер дескриптора на графической карте
+        int BasicVertexShader; // Адрес вершинного шейдера  
+        int BasicFragmentShader; // Адрес фрагментного шейдера
+
+        private void InitShaders()
+        {
+            //создание объекта программы
+            BasicProgramID = GL.CreateProgram();
+
+            //создаем объекты шейдеров, компилируем их и компонуем в объекте шейдерной программы
+            loadShader("..\\..\\Shaders\\raytracing.vert", ShaderType.VertexShader, BasicProgramID, out BasicVertexShader);
+            loadShader("..\\..\\Shaders\\raytracing.frag", ShaderType.FragmentShader, BasicProgramID, out BasicFragmentShader);
+
+            //компоновка программы
+            GL.LinkProgram(BasicProgramID);
+
+            // проверяем успех компановки
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+            //если на этом этапе status != 1, то присутствует ошибка в коде
+            if (status != 1)
+                throw new Exception("Ошибка компоновки. Исправьте ошибки в шейдерной программе.");
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID)); //вывод ошибок или предупреждений
+            GL.Enable(EnableCap.Texture2D);
+        }
+        void loadShader(String filename, ShaderType type, int program, out int address) //загрузка шейдера
+        {
+            address = GL.CreateShader(type); // //создаем объект шейдера, аргумент - тип шейдера, возвращает ссылку на шейдер
+            using (StreamReader sr = new StreamReader(filename)) //создаем стримридер для чтения
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+                // загружает исходный код в созданный шейдерный объект
+            }
+            GL.CompileShader(address); // компилирование шейдера
+            GL.AttachShader(program, address); // компоновка в шейдерную программу, подключение шейдера
+            //На этапе компоновки производится стыковка входных переменных одного шейдера с выходными переменными другого.
+            //На эпате компоновки происходит стыковка входных / выходных переменных шейдеров с соответствующими областями памяти в окружении OpenGL
+            Console.WriteLine(GL.GetShaderInfoLog(address)); //вывод, лог компиляции может указывать ошибки или предупреждение
+        }
+        private void Draw() //отрисовка
+        {
+            //очистка цветого буфера, используя цвет, заданный в clearcolor
+            GL.ClearColor(Color.AliceBlue);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+;
+            GL.UseProgram(BasicProgramID); //используем шейдерную программу
+            // вычисляем индекс переменной шейдера
+            //Камера
+            int index = GL.GetUniformLocation(BasicProgramID, "uCamera.Position"); 
+            GL.Uniform3(index, new Vector3(0, 0, -7.5f));
+            index = GL.GetUniformLocation(BasicProgramID, "uCamera.Up");
+            GL.Uniform3(index, Vector3.UnitY);
+            index = GL.GetUniformLocation(BasicProgramID, "uCamera.Side");
+            GL.Uniform3(index, Vector3.UnitX);
+            index = GL.GetUniformLocation(BasicProgramID, "uCamera.View");
+            GL.Uniform3(index, Vector3.UnitZ);
+            index = GL.GetUniformLocation(BasicProgramID, "uCamera.Scale");
+            GL.Uniform2(index, new Vector2(1, (float)glControl1.Height / glControl1.Width));
+            // Свет
+            index = GL.GetUniformLocation(BasicProgramID, "uLight.Position");
+            GL.Uniform3(index, new Vector3(2.0f, 0.0f, -4.0f));
+
+            // Квадрат
+            GL.Color3(Color.White);
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(-1, -1);
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(1, -1);
+
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(1, 1);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(-1, 1);
+
+            GL.End();
+            glControl1.SwapBuffers();
+            GL.UseProgram(0);
+
+        }
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(); //рисуем объекты
+        }
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            InitShaders(); //собираем код шейдеров
+        }
+    } 
+}
